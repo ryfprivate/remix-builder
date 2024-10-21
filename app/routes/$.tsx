0ebcf34ca101ff4c43654c4e0ce0846a3393583f
Ray Feng <ray@dotdothorse.com>
2024-10-21 20:59:07 +1100

    Added params page join

--- app/routes/$.tsx
+++ app/routes/$.tsx
@@ -13,7 +13,7 @@ export const loader = async ({ params, request, context }: LoaderArgs) => {
   const page = await builder
     .get("page", {
       userAttributes: {
-        urlPath: "/" + params["*"],
+        urlPath: "/" + (params.page?.join("/") || ""),
       },
       locale: params.locale,
     })
